dc046dc5feab1bd1ebfea22113ec7ddf8c8245d4
Liam Mueller <liamsimulate@gmail.com>
2017-11-12 03:35:47 -0600

    Check for collisions.

--- Game.py
+++ Game.py
@@ -18,7 +18,7 @@ class Game:
 
     Display.SetCaption(Config.Get("GameSettings")["Caption"])
 
-    self.Clock = Clock().SetTick(60)
+    self.Clock = Clock().SetTick(int(Config.Get("GameSettings")["Fps"]))
 
     self.__gameObjects = []
 
@@ -33,15 +33,43 @@ class Game:
         if event.type == pygame.QUIT:
           self.Quit()
 
+      self.CheckCollisions()
+
       keys = pygame.key.get_pressed()
 
       self.Clock.Tick()
 
-    Display.Update()
+      Display.Update()
 
   def CheckCollisions(self):
-    self.x = 0
+    objs = self.__gameObjects
+
+    for firstObject in range(len(objs)):
+      for secondObject in range(i, len(objs)):
+        collisionState = CollisionState()
+
+        if (objs[firstObject].x > objs[secondObject].x and objs[firstObject].x < objs[secondObject].x + objs[secondObject].width):
+          collisionState.Left = True
+        elif (objs[firstObject].x + objs[firstObject].width > objs[secondObject].x and objs[firstObject].x + objs[firstObject].width < objs[secondObject].x + objs[secondObject].width):
+          collisionState.Right = True
+
+        # We must not be colliding on the left or right
+        if (collisionState.Left or collisionState.Right):
+          continue
+
+        if (objs[firstObject].y > objs[secondObject].y and objs[secondObject].y < objs[secondObject].y + objs[secondObject].height):
+          collisionState.Top = True
+        elif (objs[firstObject].y + objs[firstObject].height > objs[secondObject].y and objs[firstObject].y + objs[firstObject].height < objs[secondObject].y + objs[secondObject].height):
+          collisionState.Bottom = True
+
+        # We must not be colliding on the top or bottom
+        if (collisionState.Top or collisionState.Bottom):
+          continue
+
+        objs[firstObject].Colliding(objs[secondObject], collisionState)
 
   def Quit(self):
     pygame.quit()
     quit()
+
+Game().Start()
