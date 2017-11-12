e5bf0ecb3396673af63b74da6aa86c8807e073de
Liam Mueller <liamsimulate@gmail.com>
2017-11-12 02:07:54 -0600

    Add config.json and Config.py to isolate configuration details programmatically.

---
+++ Config.py
@@ -0,0 +1,23 @@
+import json
+
+class Config:
+  def __init__(self):
+    raise NotImplementedError('This is a singleton. It may not be instantiated. Used "Init" instead, doofus.')
+
+  @classmethod
+  def Init(cls):
+    if (cls.__inited):
+      return
+
+    configFile = open('config.json', 'r')
+    cls.__data = json.load(configFile)
+    cls.__inited = True
+
+  @classmethod
+  def Get(cls, key):
+    return cls.__data[key]
+
+  @classmethod
+  def Reset(cls, key):
+    cls.__inited
+    cls.Init()
